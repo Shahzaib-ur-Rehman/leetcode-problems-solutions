483b5113f3db91afe7d4ee5fa119987fd2c9c3ed
Shahzaib-ur-Rehman <shahzaib@hq.crunchcraft.com>
2024-08-24 20:43:43 +0500

    876 middle of the linked list

--- middle-of-the-linked-list.js
+++ middle-of-the-linked-list.js
@@ -1,8 +1,10 @@
 var middleNode = function (head) {
-  let middle =Math.floor( head.length / 2);
-  let result = head.splice(middle)
-  return result
+    let middle =head;
+    let end=head;
+    while(end!==null && end.next!==null){
+        middle=middle.next;
+        end=end.next.next;
+    }
+    return middle
 };
 
-console.log(middleNode([1, 2, 3, 4, 5]));
-console.log(middleNode([1,2,3,4,5,6]));
